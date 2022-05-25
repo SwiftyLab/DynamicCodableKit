3b6628f85f3d0b4d04b97e06520d82bb71abae88
Soumya Ranjan Mahunt <soumya.mahunt@gmail.com>
2022-05-25 21:55:23 +0530

    chore: refactor test data

--- Tests/DynamicCodableKitTests/DynamicDecodingContextCodingKey/Models/DynamicBaseType.swift
+++ Tests/DynamicCodableKitTests/DynamicDecodingContextCodingKey/Models/DynamicBaseType.swift
@@ -4,77 +4,49 @@ import XCTest
 extension Int: DynamicDecodable {}
 extension String: DynamicDecodable {}
 
-struct VariableBaseDataTypeContainer: Decodable {
-    @DynamicDecodingWrapper<CodingKeys> var value: Decodable
+enum DynamicBaseDataTypeCodingKeys: String, CodingKey, DynamicDecodingContextCodingKey {
+    typealias Identified = Decodable
+    case value
 
-    enum CodingKeys: String, CodingKey, DynamicDecodingContextCodingKey {
-        typealias Identified = Decodable
-        case value
-
-        static func context<Container: KeyedDecodingContainerProtocol>(
-            forContainer container: Container
-        ) throws -> DynamicDecodingContext<Identified> where CodingKeys == Container.Key {
-            return DynamicDecodingContext(decoding: Int.self, fallback: String.self)
-        }
+    static func context<Container: KeyedDecodingContainerProtocol>(
+        forContainer container: Container
+    ) throws -> DynamicDecodingContext<Identified> where Self == Container.Key {
+        return DynamicDecodingContext(decoding: Int.self, fallback: String.self)
     }
+}
+
+enum DynamicBaseDataTypeCollectionCodingKeys: String, CodingKey, DynamicDecodingContextCodingKey {
+    typealias Identified = Decodable
+    case values
+
+    static func context<Container: KeyedDecodingContainerProtocol>(
+        forContainer container: Container
+    ) throws -> DynamicDecodingContext<Identified> where Self == Container.Key {
+        return DynamicDecodingContext(decoding: Int.self, fallback: String.self)
+    }
+}
+
+struct VariableBaseDataTypeContainer: Decodable {
+    typealias CodingKeys = DynamicBaseDataTypeCodingKeys
+    @DynamicDecodingWrapper<CodingKeys> var value: Decodable
 }
 
 struct OptionalVariableBaseDataTypeContainer: Decodable {
+    typealias CodingKeys = DynamicBaseDataTypeCodingKeys
     @OptionalDynamicDecodingWrapper<CodingKeys> var value: Decodable?
-
-    enum CodingKeys: String, CodingKey, DynamicDecodingContextCodingKey {
-        typealias Identified = Decodable
-        case value
-
-        static func context<Container: KeyedDecodingContainerProtocol>(
-            forContainer container: Container
-        ) throws -> DynamicDecodingContext<Identified> where CodingKeys == Container.Key {
-            return DynamicDecodingContext(decoding: Int.self, fallback: String.self)
-        }
-    }
 }
 
 struct StrictVariableBaseDataTypeContainer: Decodable {
+    typealias CodingKeys = DynamicBaseDataTypeCollectionCodingKeys
     @StrictDynamicDecodingArrayWrapper<CodingKeys> var values: [Decodable]
-
-    enum CodingKeys: String, CodingKey, DynamicDecodingContextCodingKey {
-        typealias Identified = Decodable
-        case values
-
-        static func context<Container: KeyedDecodingContainerProtocol>(
-            forContainer container: Container
-        ) throws -> DynamicDecodingContext<Identified> where CodingKeys == Container.Key {
-            return DynamicDecodingContext(decoding: Int.self, fallback: String.self)
-        }
-    }
 }
 
 struct DefaultVariableBaseDataTypeContainer: Decodable {
+    typealias CodingKeys = DynamicBaseDataTypeCollectionCodingKeys
     @DefaultValueDynamicDecodingArrayWrapper<CodingKeys> var values: [Decodable]
-
-    enum CodingKeys: String, CodingKey, DynamicDecodingContextCodingKey {
-        typealias Identified = Decodable
-        case values
-
-        static func context<Container: KeyedDecodingContainerProtocol>(
-            forContainer container: Container
-        ) throws -> DynamicDecodingContext<Identified> where CodingKeys == Container.Key {
-            return DynamicDecodingContext(decoding: Int.self, fallback: String.self)
-        }
-    }
 }
 
 struct LossyVariableBaseDataTypeContainer: Decodable {
+    typealias CodingKeys = DynamicBaseDataTypeCollectionCodingKeys
     @LossyDynamicDecodingArrayWrapper<CodingKeys> var values: [Decodable]
-
-    enum CodingKeys: String, CodingKey, DynamicDecodingContextCodingKey {
-        typealias Identified = Decodable
-        case values
-
-        static func context<Container: KeyedDecodingContainerProtocol>(
-            forContainer container: Container
-        ) throws -> DynamicDecodingContext<Identified> where CodingKeys == Container.Key {
-            return DynamicDecodingContext(decoding: Int.self, fallback: String.self)
-        }
-    }
 }
