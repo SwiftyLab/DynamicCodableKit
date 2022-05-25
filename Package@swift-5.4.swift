3edf5dea5ace912c057cc42c6c0dda69c7010008
Soumya Ranjan Mahunt <soumya.mahunt@gmail.com>
2022-05-25 17:24:21 +0530

    chore: add muti swift tool manifest files

---
+++ Package@swift-5.4.swift
@@ -0,0 +1,35 @@
+// swift-tools-version:5.1
+
+import PackageDescription
+
+let package = Package(
+    name: "dynamic-codable-kit",
+    platforms: [
+        .iOS(.v8),
+        .macOS(.v10_10),
+        .tvOS(.v9),
+        .watchOS(.v2)
+    ],
+    products: [
+        .library(
+            name: "DynamicCodableKit",
+            targets: ["DynamicCodableKit"]
+        ),
+    ],
+    targets: [
+        .target(
+            name: "DynamicCodableKit",
+            dependencies: []
+        ),
+        .testTarget(
+            name: "DynamicCodableKitTests",
+            dependencies: ["DynamicCodableKit"],
+            resources: [
+                .process("DynamicDecodingContextCodingKey/JSONs"),
+                .process("DynamicDecodingContextContainerCodingKey/JSONs"),
+                .process("DynamicDecodingContextProvider/JSONs"),
+            ]
+        ),
+    ],
+    swiftLanguageVersions: [.v5]
+)
