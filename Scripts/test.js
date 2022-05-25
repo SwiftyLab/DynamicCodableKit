674c274bc8c2939bdc52735575c9cee10a2b3df9
Soumya Ranjan Mahunt <soumya.mahunt@gmail.com>
2022-05-25 20:03:48 +0530

    chore: enable `--enable-test-discovery` flag to fix test error

--- Scripts/test.js
+++ Scripts/test.js
@@ -4,7 +4,9 @@ const { execSync } = require('node:child_process');
 
 const args = process.argv.slice(2).join(' ');
 execSync(
-  `swift test ${args} --verbose --enable-code-coverage`, {
+  `swift test ${args} --verbose \
+    --enable-code-coverage \
+    --enable-test-discovery`, {
     stdio: ['inherit', 'inherit', 'inherit'],
     encoding: 'utf-8'
   }
