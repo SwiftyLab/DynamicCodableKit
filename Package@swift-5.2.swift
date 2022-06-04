19044fd8c2f5092f44e77d7143ee62ebb769853d
Soumya Ranjan Mahunt <soumya.mahunt@gmail.com>
2022-06-04 09:50:30 +0530

    Revert "chore: add single major swift tool package manifest"
    This reverts commit b0316755f7656ad3c69e7c744ee414f9c0c1f053.

---
+++ Package@swift-5.2.swift
@@ -0,0 +1,30 @@
+// swift-tools-version:5.2
+
+import PackageDescription
+
+let package = Package(
+    name: "DynamicCodableKit",
+    platforms: [
+        .iOS(.v8),
+        .macOS(.v10_10),
+        .tvOS(.v9),
+        .watchOS(.v2)
+    ],
+    products: [
+        .library(
+            name: "DynamicCodableKit",
+            targets: ["DynamicCodableKit"]
+        ),
+    ],
+    targets: [
+        .target(
+            name: "DynamicCodableKit",
+            dependencies: []
+        ),
+        .testTarget(
+            name: "DynamicCodableKitTests",
+            dependencies: ["DynamicCodableKit"]
+        ),
+    ],
+    swiftLanguageVersions: [.v5]
+)
